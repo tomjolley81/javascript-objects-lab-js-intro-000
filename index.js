b8236b6043b14c200b2af456e4c9a94d1f78e585
Thomas Jolley <tomjolley81@gmail.com>
2018-11-07 21:27:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var recipes = {}
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign(object, { [key]: value })
+  return object
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {}
+function deleteFromObjectByKey(object, key) {}
+function destructivelyDeleteFromObjectByKey(object, key) {}
